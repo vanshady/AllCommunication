bccf8c5bd25568ffb1db9159c7fb30466becb7ce
adamxu <axislegend@gmail.com>
2016-09-10 14:54:58 -0400

    Stylistic edits

--- scripts/VideoMerger.sh
+++ scripts/VideoMerger.sh
@@ -14,8 +14,8 @@ cd temp
 
 for url in "$@"
 do
-    if !([[ $url == https://www.signingsavvy.com/signs/mp4/* ]])
-        then continue
+    if !([[ $url == https://www.signingsavvy.com/signs/mp4/* ]]) then
+        continue
     fi
 
     curl -O $url
@@ -23,7 +23,8 @@ done
 
 for file in *.mp4
 do
-    ffmpeg -y -i $file -c copy -bsf:v h264_mp4toannexb -f mpegts intermediate_${file}.ts
+    ffmpeg -y -i $file -c copy -bsf:v h264_mp4toannexb \
+           -f mpegts intermediate_$file.ts
 done
 
 intermediates=""
@@ -34,14 +35,12 @@ done
 
 outputName=${@: -1}
 
-if ([[ ${outputName} == https://www.signingsavvy.com/signs/mp4/* ]]) then
+if ([[ $outputName == https://www.signingsavvy.com/signs/mp4/* ]]) then
     outputName="output.mp4"
     echo "[WARNING] No ouput file name specified. Outputs to output.mp4."
 fi
 
-ffmpeg -y -i "concat:${intermediates}" -c copy ../output/${outputName}
+ffmpeg -y -i "concat:${intermediates}" -c copy ../output/$outputName
 
 rm *.mp4
 rm intermediate_*.ts
-
-# https://www.signingsavvy.com/signs/mp4/7/7449.mp4 https://www.signingsavvy.com/signs/mp4/7/7448.mp4
