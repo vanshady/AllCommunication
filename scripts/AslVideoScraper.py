1d020158bf81a710e4e4c4d0aa2654b58ce8df9c
Minwei Xu <faceswilliam@gmail.com>
2016-09-13 14:27:33 -0400

    asl output

--- scripts/AslVideoScraper.py
+++ scripts/AslVideoScraper.py
@@ -68,3 +68,5 @@ def getWebpageSource(url):
 # checks if a given string contains only letters or apostrophes
 def isWord(word):
     return not any(c for c in word if c not in ascii_letters + "'")
+
+print(getVideoUrl(sys.argv[1]))
